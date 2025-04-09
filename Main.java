cf77e8adeecabb772d7f88e4de0fcf6f1c2893c7
slvzz <slvgrzz@gmail.com>
2025-04-09 13:49:06 -0300

    finalizando o projeto

--- Main.java
+++ Main.java
@@ -1,14 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-    Livro livro1 = new Livro("Teste", "silvio", true);
-    Livro livro2 = new Livro("Romeu e julieta", "guzera", true);
-    Livro livro3 = new Livro("Sexo", "lua", true);
+    Livro livro1 = new Livro("testando testes", "silvio", true);
+    Livro livro2 = new Livro("Romeu e julieta", "shakespeare", true);
+    Livro livro3 = new Livro("documentario lua", "lua", true);
 
     Biblioteca biblioteca = new Biblioteca();
 
     biblioteca.adicionarLivro(livro1);
     biblioteca.adicionarLivro(livro2);
     biblioteca.adicionarLivro(livro3);
+
+    System.out.println(biblioteca.listarLivros());
+
+    biblioteca.emprestarLivro(livro3);
+
+    System.out.println(biblioteca.listarLivros());
+
+    biblioteca.devolverLivro(livro3);
 
     System.out.println(biblioteca.listarLivros());
     }
